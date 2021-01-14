9ecf6233e09b603c84a8ab6ce5cff6e78243ce23
Gustavo Gutierrez <gusguti04@gmail.com>
2021-01-13 18:16:22 -0800

    Finish piece functions

--- ReversiJS/src/piece.js
+++ ReversiJS/src/piece.js
@@ -2,18 +2,29 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    // if ( this.color === "black") {
+    //     return "white";
+    // } else if (this.color === "white") {
+    //     return "black"
+    // }
+    // if ( this.color === "black" ) return "white";
+    // if ( this.color === "white" ) return "black";
+    // return (this.color === 'black') ? 'white' : 'black';
+    return (this.color === "black") ? "white" : "black";
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+   this.color = this.oppColor();
 };
 
 /**
@@ -21,6 +32,8 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+    if ( this.color === "white" ) return "W";
+    if ( this.color === "black" ) return "B";
 };
 
 // DON'T TOUCH THIS CODE
